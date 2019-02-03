0a95c157c23d0a7abc569ed3ee336f8a075bd4e5
Jordan Lawrence <jordanl17@me.com>
2019-02-03 20:01:08 +0000

    mockEvents added

---
+++ src/mockEvents.js
@@ -0,0 +1,19 @@
+const mock = {
+  colours: {
+    primary: 'some colour'
+  },
+  lifeEvents: [
+    {
+      id: 0,
+      title: 'An Education event',
+      category: 'Education'
+    },
+    {
+      id: 1,
+      title: 'An Industry event',
+      category: 'Industry'
+    }
+  ]
+};
+
+export default mock;
