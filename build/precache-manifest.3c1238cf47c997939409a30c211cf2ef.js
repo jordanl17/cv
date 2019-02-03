cd788bfd2a0fc9d4e874577cdd82152430a81832
Jordan Lawrence <jordanl17@me.com>
2019-02-03 18:07:30 +0000

    terser version downgrade

---
+++ build/precache-manifest.3c1238cf47c997939409a30c211cf2ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f395554e46d0d05b44b7",
+    "url": "/static/js/main.f395554e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f395554e46d0d05b44b7",
+    "url": "/static/css/main.4102ee01.chunk.css"
+  },
+  {
+    "revision": "d91a12d6b4cad132c16a35a885641471",
+    "url": "/index.html"
+  }
+];
